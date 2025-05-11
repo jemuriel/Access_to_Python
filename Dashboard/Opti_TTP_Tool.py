1ae54b69cf7bb471820600e6f0f9f745575c3912
Juan Muriel <jemuriel1@gmail.com>
2025-05-11 16:00:17 +0800

    WIP

--- Dashboard/Opti_TTP_Tool.py
+++ Dashboard/Opti_TTP_Tool.py
@@ -3,61 +3,33 @@ import os
 import streamlit as st
 
 
+import streamlit as st
+
 def main() -> None:
-
-    # app configs
-    st.set_page_config(layout="wide")  # use all the available space
-
-    image_path = "Images/pacific-national.webp"
-    st.image(image_path, width=200)
-
-    st.markdown(
-        """
-        # TTP Optimisation Tool
-
-
-        #### 👈 Select a tab from the sidebar
-
-    """
+    # Page settings
+    st.set_page_config(
+        page_title="TTP Optimisation Tool",
+        layout="wide",
+        page_icon="🚆",
     )
-    # scenarios = ["standard outputs"] + sorted(
-    #     [
-    #         x
-    #         for x in os.listdir("data/scenarios")
-    #         if os.path.isdir(os.path.join("data", "scenarios", x))
-    #     ]
-    # )
-    #
-    # def refresh_data() -> None:
-    #     for key in st.session_state.keys():
-    #         if key not in ["output_folder", "chosen_scenario"]:
-    #             del st.session_state[key]
-    #
-    # chosen_scenario = st.session_state.get("chosen_scenario")
-    # if chosen_scenario:
-    #     scenarios = [chosen_scenario] + [x for x in scenarios if x != chosen_scenario]
-    # chosen_scenario = st.selectbox(
-    #     "scenario", options=scenarios, on_change=refresh_data
-    # )
-    # if chosen_scenario:
-    #     st.session_state["chosen_scenario"] = chosen_scenario
-    #     st.session_state["input_folder"] = (
-    #         os.path.join("data", "scenarios", chosen_scenario, "inputs")
-    #         if chosen_scenario != "standard outputs"
-    #         else os.path.join("data", "inputs")
-    #     )
-    #     st.session_state["data_model_folder"] = (
-    #         os.path.join("data", "scenarios", chosen_scenario, "data_model")
-    #         if chosen_scenario != "standard outputs"
-    #         else os.path.join("data", "data_model")
-    #     )
-    #     st.session_state["output_folder"] = (
-    #         os.path.join("data", "scenarios", chosen_scenario, "outputs")
-    #         if chosen_scenario != "standard outputs"
-    #         else os.path.join("data", "outputs")
-    #     )
-    #     st.session_state["src_folder"] = "src"
 
+    # Centered layout
+    col1, col2, col3 = st.columns([1, 2, 1])
+
+    with col2:
+        # Better quality logo from Pacific National site
+        st.image("Images/pn_loco.jpg", use_container_width=True)
+
+        st.markdown(
+            """
+            <div style='text-align: center; padding-top: 20px;'>
+                <h1 style='font-size: 42px; color: #003366;'>TTP Optimisation Tool</h1>
+                <h4 style='color: #FFFFFF;'>🚆 Tactical Train Planning</h4>
+                <p style='margin-top: 30px; font-size: 16px; color: #FFFFFF;'>👈 Use the sidebar to begin navigating through the tool.</p>
+            </div>
+            """,
+            unsafe_allow_html=True,
+        )
 
 if __name__ == "__main__":
     main()
